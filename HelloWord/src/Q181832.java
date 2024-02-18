03d10d7824272c872b09a872c1bc085a9224d025
parkeunvin <eunbin7372@naver.com>
2024-02-18 19:15:53 +0900

    정수를 나선형으로 배치하기

--- HelloWord/src/Q181832.java
+++ HelloWord/src/Q181832.java
@@ -1,12 +1,62 @@
 public class Q181832 {
     public static void main(String[] args){
-        System.out.println(solution(4));
+        solution(4);
 
     }
 
         public static int[][] solution(int n) {
-            int[][] answer = {};
-            int a;
+            int[][] answer = new int[n][n];
+            int row=0;
+            int col=0;
+            int count = 1;
+            String change = "right";
+
+            while (count <= n * n) {
+                answer[row][col] = count++;
+                
+                switch (change) {
+                    case "right": 
+                        if (col + 1 < n && answer[row][col + 1] == 0) {
+                            col++;
+                        } else { 
+                            change = "down";
+                            row++;
+                        }
+                        break;
+                    case "down": 
+                        if (row + 1 < n && answer[row + 1][col] == 0) {
+                            row++;
+                        } else { 
+                            change = "left";
+                            col--;
+                        }
+                        break;
+                    case "left": 
+                        if (col - 1 >= 0 && answer[row][col - 1] == 0) {
+                            col--;
+                        } else {
+                            change = "up";
+                            row--;
+                        }
+                        break;
+                    case "up":
+                        if (row - 1 >= 0 && answer[row - 1][col] == 0) {
+                            row--;
+                        } else { 
+                            change = "right";
+                            col++;
+                        }
+                        break;
+                }
+            }
+
+                        for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    System.out.print(answer[i][j] + " ");
+                }
+                System.out.println();
+            }
+
             return answer;
         }
 }
