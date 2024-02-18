d696bfb327a39e13ac64910b838aa7090c1ece8a
parkeunvin <eunbin7372@naver.com>
2024-02-18 17:05:30 +0900

    정수를 나선형으로 배치

---
+++ HelloWord/src/Q181832.java
@@ -0,0 +1,12 @@
+public class Q181832 {
+    public static void main(String[] args){
+        System.out.println(solution(4));
+
+    }
+
+        public static int[][] solution(int n) {
+            int[][] answer = {};
+            int a;
+            return answer;
+        }
+}
