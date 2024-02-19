fbc3adeb5f64dd948f654d36c22cdf09be40b4e8
parkeunbin <eunbin7372@naver.com>
2024-02-19 17:37:22 +0900

    삼각형

--- HelloWord/src/Q120868.java
+++ HelloWord/src/Q120868.java
@@ -9,8 +9,18 @@ public class Q120868 {
 
     public static int solution(int[] sides) {
         int answer = 0;
+        int max=0;
+        int min=0;
 
-        
+        if(sides[0]>sides[1]){
+            max=sides[0];
+            min=sides[1];
+        }else{
+            max=sides[1];
+            min=sides[0];
+        }
+
+        for(int i=min; i<max)        
 
 
 
